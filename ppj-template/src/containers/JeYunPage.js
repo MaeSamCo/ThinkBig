b0a61b150db2f00d67411a373015bffe3636b52f
jeyun0 <jeyun0@naver.com>
2018-10-25 21:51:17 +0900

    약간바뀜

--- ppj-template/src/containers/JeYunPage.js
+++ ppj-template/src/containers/JeYunPage.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import styled, { css } from 'styled-components'
 import {PrettyButton, PB} from '../components/PrettyButton'
+import './JeYunPage.css'
 
 class JeYunPage extends Component {
+
     render() {
         return (
             <div id="je-yun-page">
-                <h1>JEYUN</h1>
+                <h1>Management Page</h1>
 
-                <PrettyButton>asadsfasdfasdfdf</PrettyButton>
-                <PB>dsfsdfmlsdfsdfkml</PB>
+                <div className="manage-box">
+                    현재인원 : <br></br>
+                    청결관리 : <br></br>
+                    시간관리 : <input type="date" readOnly></input>
+                </div>
             </div>
+            
         )
     }
 }
