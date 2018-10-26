b26b96ec3227276ae0268063746f38506312e823
aldosodo <44400433+aldosodo@users.noreply.github.com>
2018-10-26 23:21:42 +0900

    Update

--- ppj-template/src/containers/JongMinPage.js
+++ ppj-template/src/containers/JongMinPage.js
@@ -1,13 +1,75 @@
 import React, { Component } from 'react'
+import { ContextHOC } from '../context/ContextMain'
+import './jongminpage.css'
 
 class JongMinPage extends Component {
-  render () {
-    return (
-      <div id='jong-min-page'>
-        <h1>JONGMIN</h1>
-      </div>
-    )
+  constructor (props) {
+    super(props)
   }
+    render() {
+        return (
+            <div id="jong-min-page" className='background'>
+                <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR" rel="stylesheet"></link>
+
+
+                <div className='navbar'>
+                    <div className='profile'>
+                        <div className='profilepic'></div>
+                        <div className='profilecir'></div>
+                        <div className='name'>{this.props.state.wow}</div>
+                        <div className='living_place'>안산 디미고</div>
+                    </div>
+                    <div className='catalog'>
+                        <ul>
+                            <li>cat1</li>
+                            <li>cat2</li>
+                            <li>cat3</li>
+                            <li>cat4</li>
+                            <li>cat5</li>
+                            <li>cat6</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className='topsearchbar'>
+                    <input className="search" placeholder=" Search                                                  ?" type="text"/>
+                    <div className='newpost'>+ Create New Post</div>
+                </div>
+
+                <div className='contents'>
+                    <h2 className='placeinfotitle'>장소정보</h2>
+                    <div className='placeinfocontents' className='placeinfocontents_1'>
+                        장소분류<br/>
+                        <div className='placecat'> 
+                          <img className='basketball'src={require('../components/basketball.png')}/>
+                        </div><br/>
+                        {this.props.state.classify}
+                    </div>
+                    <div className='placeinfocontents' className='placeinfocontents_2'>
+                        장소평가<br/> <h1 className='stars'>
+                          {this.props.state.star>0&&'★'}
+                          {this.props.state.star>1&&'★'}
+                          {this.props.state.star>2&&'★'}
+                          {this.props.state.star>3&&'★'}
+                          {this.props.state.star>4&&'★'}
+                          {this.props.state.star<5&&'☆'}
+                          {this.props.state.star<4&&'☆'}
+                          {this.props.state.star<3&&'☆'}
+                          {this.props.state.star<2&&'☆'}
+                        </h1><br/>{this.props.state.star}/5
+                    </div>
+                    <div className='placeinfocontents' className='placeinfocontents_3'>
+                        권장인원<br/>
+                        {[...Array(this.props.state.max_people)].map(v => <img className='person'src={require('../components/person.png')}/>)}
+                        <br/> {this.props.state.max_people}명
+                    </div>
+                    <h2 className='nowinfotitle'>현재정보</h2>
+                    <div className='placeinfocontents' className='placeinfocontents_4'></div>
+                </div>
+                
+            </div>
+        )
+    }
 }
 
-export default JongMinPage
+export default ContextHOC(JongMinPage)
--- ppj-template/src/context/ContextMain.js
+++ ppj-template/src/context/ContextMain.js
@@ -17,10 +17,61 @@ class ContextProvider extends Component {
             this.setState({wow: snapshot.val()})
             // console.log(snapshot.val())
         })
+        getFireDB('/place/dimigobasketball/additional_condition')
+        .on('value', (snapshot) => {
+            this.setState({additional_condition: snapshot.val()})
+            // console.log(snapshot.val())
+        })
+        getFireDB('/place/dimigobasketball/additional_people')
+        .on('value', (snapshot) => {
+            this.setState({additional_people: snapshot.val()})
+            // console.log(snapshot.val())
+        })
+        getFireDB('/place/dimigobasketball/max_people')
+        .on('value', (snapshot) => {
+            this.setState({max_people: snapshot.val()})
+            // console.log(snapshot.val())
+        })
+        getFireDB('/place/dimigobasketball/playing_number')
+        .on('value', (snapshot) => {
+            this.setState({playing_number: snapshot.val()})
+            // console.log(snapshot.val())
+        })
+        getFireDB('/place/dimigobasketball/recommanded_time')
+        .on('value', (snapshot) => {
+            this.setState({recommanded_time: snapshot.val()})
+            // console.log(snapshot.val())
+        })
+        getFireDB('/place/dimigobasketball/star')
+        .on('value', (snapshot) => {
+            this.setState({star: snapshot.val()})
+            // console.log(snapshot.val())
+        })
+        getFireDB('/place/dimigobasketball/classify')
+        .on('value', (snapshot) => {
+            this.setState({classify: snapshot.val()})
+            // console.log(snapshot.val())
+        })
+        //     while(getFireDB('/place/dimigobasketball/reservationlist_'+num)=!''){
+        //         .on(getFireDB('/place/dimigobasketball/reservationlist_'+num)=!''&&'value', (snapshot) => {
+        //             this.setState({recommanded_time+num: snapshot.val()})
+        //             // console.log(snapshot.val())
+        //             num++
+        //     }
+        // }
+        
     }
 
     state = {
-        wow: ''
+        wow: '',
+        additional_condition:'',
+        additional_people:'',
+        max_people:'',
+        playing_number:'',
+        recommanded_time:'',
+        reservationlist_1:'',
+        star:'',
+        classify:''
     }
 
     actions = {
