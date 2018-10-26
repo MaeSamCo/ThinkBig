adde657a09cf18235279f3785dfa9110d37587a2
jeyun0 <jeyun0@naver.com>
2018-10-26 23:08:52 +0900

    모달수정

--- ppj-template/src/components/Modal.js
+++ ppj-template/src/components/Modal.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types';
 
 const modalRoot = document.getElementById('modal-root');
 
+    // The gray background
+    const backdropStyle = {
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        padding: 50
+    };
+
+    // The modal "window"
+    const modalStyle = {
+        
+        display: 'flex',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        maxWidth: 400,
+        minHeight: 300,
+        margin: '0 auto',
+        padding: 30,
+        justifyContent: 'space-between',
+        fontFamily: '나눔고딕',
+        fontSize: '16px'
+    };
+
+    /* The Close Button */
+    const close = {
+        color: '#aaaaaa',
+        float: 'right',
+        fontSize: '28px',
+        fontWeight: 'bold',
+    }
+
+    
 class Modal extends React.Component {
     constructor(props) {
         super(props);
         this.el = document.createElement('div');
     }
+    
 
     // componentDidMount() {
     //     // The portal element is inserted in the DOM tree after
@@ -27,8 +66,26 @@ class Modal extends React.Component {
 
     render() {
         // Render nothing if the "show" prop is false
-        if(!this.props.show) {
-        return null;
+        // if(!this.props.show) {
+        //     return null;
+        // }
+        
+        return ReactDOM.createPortal(
+            <div className="backdrop" style={backdropStyle}>
+                <div className="modal" style={modalStyle}>
+                    <div>{this.props.children}</div>
+                    <span className="close" onClick={this.props.onClose} style={close}>&times;</span>
+                    {/* <div className="footer">
+                    
+                    <button onClick={this.props.onClose}>
+                    Close
+                    </button>
+                </div> */}
+                </div>
+            </div>
+            ,
+            this.el
+        );
     }
 
 
@@ -38,12 +95,6 @@ class Modal extends React.Component {
     //     textDecoration: 'none';
     //     cursor: 'pointer';
     // }
-
-    return ReactDOM.createPortal(
-            this.props.children,
-            this.el
-    );
-  }
 }
 
 Modal.propTypes = {
