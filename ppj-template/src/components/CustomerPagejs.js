30c6d6179b9032c872b3395268e2349455fe84cd
gun2jang <firstunderbar@gmail.com>
2018-10-26 21:53:09 +0900

    little change

--- ppj-template/src/components/CustomerPagejs.js
+++ ppj-template/src/components/CustomerPagejs.js
@@ -1,6 +1,7 @@
 // import React from 'react';
 // import PropTypes from 'prop-types';
 
+<<<<<<< HEAD
 // class Customerpage extends React.Component {
 //     let hideMenu = document.getElementById('hideMenu')
 //     const hideButton = document.getElementById('HideandShow')
@@ -12,5 +13,18 @@
 //         }
 //     })
 // }
+=======
+class Customerpage extends React.Component {
+    // let hideMenu = document.getElementById('hideMenu')
+    // const hideButton = document.getElementById('HideandShow')
+    // hideButton.addEventListener('click', ()=>{
+    //     if(hideMenu.style.display === 'none'){
+    //         hideMenu.style.display = 'inline-block'
+    //     } else{
+    //         hideMenu.style.display = 'none'
+    //     }
+    // })
+}
+>>>>>>> cd16e78779507fd0254676f49e47804bac3c1bba
 
 // export default Customerpage
--- ppj-template/src/containers/Management.js
+++ ppj-template/src/containers/Management.js
@@ -2,25 +2,52 @@ import React, { Component } from 'react'
 import styled, { css } from 'styled-components'
 import {PrettyButton, PB} from '../components/PrettyButton'
 import '../components/Management.css'
+import Modal from '../components/Modal';
 
 class Management extends Component {
 
+<<<<<<< HEAD
     showAlert() {
     }
+=======
+    constructor(props) {
+        super(props);
+        
+        this.state = { isOpen: false, currentIndex: 0,
+            jangso: {name: ["꽃빛공원", "디미고", "딤이고","한국디지털미디어고등학교 체육관", "우와"],
+            people: [5,3,1,4,6],
+            clean: ["청결","더러움","보통","보통","보통"],
+            temperatue: [30, 12,60,100,20]}
+         }
+    }
+
+    toggleModal = () => {
+        this.setState({
+            isOpen: !this.state.isOpen,
+            currentIndex: 0,
+            jangso: {name: ["꽃빛공원", "디미고", "딤이고","한국디지털미디어고등학교 체육관", "우와"],
+            people: [5,3,1,4,6],
+            clean: ["청결","더러움","보통","보통","보통"],
+            temperatue: [30, 12,60,100,20]}
+        });
+        // </div><div className="single-content">현재인원 : {this.state.jangso['people'][0]}</div>
+        // <div className="single-content">청결관리 : {this.state.jangso['clean'][0]}</div>
+        // <div className="single-content">시간관리 : <input type="time"></input></div>
+        // <div className="single-content">기온 : {this.state.jangso['temperatue'][0]}℃</div>)
+        ReactDOM.render(<div className="modal-title">{this.state.jangso['name'][0]}</div>, document.getElementById('modal'))
+    }
+
+>>>>>>> cd16e78779507fd0254676f49e47804bac3c1bba
     render() {        
-        const jangso= ["꽃빛공원", "디미고", "딤이고","한국디지털미디어고등학교 체육관", "우와"];
+
         return (
             <div>   
-
                 <div className="upper-bar">
                     <div className="upper-bar-header">
                         <a href="#">Management Page</a>
                         <div className="upper-bar-right">관리자 <i className="fas fa-angle-down"></i></div>
                     </div>
                 </div>
-                <dialog className="detailed-box">
-                    <p>여러분 안녕하세요!</p>
-                </dialog>
                 <div>
                     <div className="all-manage-box">
                         <div className="manage-location">
@@ -31,22 +58,24 @@ class Management extends Component {
 
                             <div className="location-list">
                                 {
-                                    jangso.map((name,index)=>                      
+                                    this.state.jangso['name'].map((name,index)=>                      
                                     <div className="location-element">
                                         <div className="location-element-title">
                                             {name}
-                                            <span className="detailed-info" id={index} onClick={this.showAlert}><i className="fas fa-info location-element-icon"></i></span>
+                                            <span className="detailed-info" id={index.toString()} onClick={this.toggleModal}><i className="fas fa-info location-element-icon"></i></span>
                                         </div>
                                     </div>
                                     )
                                 }
                             </div>
                         </div>
-                        현재인원 : <br></br>
-                        청결관리 : <br></br>
-                        시간관리 : <input type="date" readOnly></input>
+
                     </div>
                 </div>
+                <Modal show={this.state.isOpen}
+                    onClose={this.toggleModal}>
+                    <div id="modal"></div>
+                </Modal>
             </div>
         )
     }
