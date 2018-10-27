87acb38b161fbd78604bdbba79648238bf979cb6
aldosodo <44400433+aldosodo@users.noreply.github.com>
2018-10-27 10:06:15 +0900

    'pyeongjinhi'

--- ppj-template/src/containers/TestPage.js
+++ ppj-template/src/containers/TestPage.js
@@ -6,14 +6,27 @@ class TestPage extends Component {
   constructor (props) {
     super(props)
   }
-
-  render () {
-    return (
-      <div id='test-page'>
-        <h1>
-          {this.props.state.wow}
-        </h1>
-      </div>
+  get(){
+    // console.log(Object.values(getFireDB('place/'+document.getElementById("find").val())))
+    console.log(getFireDB('place/basketball'))
+  }
+    render() {
+        return (
+            <div id="test-page">
+                <h1>
+                    {this.props.state.wow}
+                </h1>
+                    장소명:<input id='name' type="text"/><br/>
+                    분류:<input id='_class' type="text"/><br/>
+                    최대인원:<input id='maxpeople' type="text"/><br/>
+                    설명:<input id='description' type="text"/><br/>
+                    권장시간:<input id='recommendtime' type="text"/><br/>
+                    위치:<input id='location' type="text"/><br/>
+                    <div className='submit' onClick={this.save}>submit</div>
+                    {/* {writeUserData(userId,name,email)} */}
+                    찾으려는것:<input id='find' type="text"/><br/>
+                    <div className='get' onClick={this.get}>get</div>
+            </div>
     )
   }
 }
